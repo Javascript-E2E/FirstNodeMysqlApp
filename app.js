aba2ae1a939f376c968531690158642b5b1cc0db
Azad Ramasamy <Azad Ramasamy>
2017-01-08 00:06:10 -0500

    adding one more service

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var connection = mysql.createPool({
     connectionLimit:50,
     host:'localhost',
     user:'root',
-    password: '1234',
+    password: 'Chandra@88',
     database: 'world'
 });
 
@@ -17,12 +17,30 @@ app.get('/getAllCounties', function(req, resp){
         }else{
             tempconnect.query('select * from country', function(error, rows, fields){
                 tempconnect.release();
+                console.log(req.body);
                  if(error){
                     resp.json(error);
                 }else{
                     resp.json(rows);
                 }
 
+            })
+        }
+    })
+})
+
+app.get('/getCountriesByContinent?:continent',function(req,resp){
+    connection.getConnection(function(error, tempconnect){
+        if(error){
+            resp.json(error);
+        }else{
+            tempconnect.query('select * from country where Continent = ?',[req.query.continent],function(error,rows,fields){
+                tempconnect.release();
+                if(error){
+                    resp.json(error);
+                }else{
+                    resp.json(rows);
+                }
             })
         }
     })

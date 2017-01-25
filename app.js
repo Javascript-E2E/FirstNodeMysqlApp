bc8bd74cd95462f8e1d968b4bc91a140a4daabbb
Valliappan, Gandhi <GANDHI_VALLIAPPAN@homedepot.com>
2017-01-25 02:06:12 -0500

    refactor node db transaction code

--- app.js
+++ app.js
@@ -1,42 +1,16 @@
 var db = require('./config/db');
 var nodeServer = require('./config/server');
 var sqlQuery = require('./queries/queries')
+var trans = require('./transactions/dbTransactions')
 
 nodeServer.start();
 
 nodeServer.app.get('/getAllcontactDetails', function(req, resp){
-    db.dbConnection.getConnection(function(error,tempconnect){
-        if(error){
-            resp.json(error);
-        }else{
-            tempconnect.query(sqlQuery.getAllcontactDetails(), function(error, rows, fields){
-                tempconnect.release();
-                 if(error){
-                    resp.json(error);
-                }else{
-                    resp.json(rows);
-                }
-
-            })
-        }
-    })
+    trans.selectQuery(sqlQuery.getAllcontactDetails(),req,resp);
 })
 
 nodeServer.app.get('/getContactBy?:phone_number',function(req,resp){
-    dbConnection.getConnection(function(error, tempconnect){
-        if(error){
-            resp.json(error);
-        }else{
-            tempconnect.query(sqlQuery.getContactBy(),[req.query.phone_number],function(error,rows,fields){
-                tempconnect.release();
-                if(error){
-                    resp.json(error);
-                }else{
-                    resp.json(rows);
-                }
-            })
-        }
-    })
+    trans.selectQuery(sqlQuery.getContactBy(),req,resp);
 })
 
 //app.listen(process.env.PORT || 5000);
---
+++ transactions/dbTransactions.js
@@ -0,0 +1,39 @@
+var db = require('../config/db');
+var sqlQuery = require('../queries/queries')
+var mysql = require('mysql');
+
+
+var selectQuery = (query,req,resp) => {
+        db.dbConnection.getConnection(function(error,tempconnect){
+        if(error){
+            resp.json(error);
+        }else{
+            var queryToBeExecuted = formatQuery(query,req.query);
+            tempconnect.query(queryToBeExecuted,function(error, rows, fields){
+                tempconnect.release();
+                 if(error){
+                    resp.json(error);
+                }else{
+                    resp.json(rows);
+                }
+
+            })
+        }
+    });
+
+    return resp;
+};
+
+var formatQuery = (query,params) => {
+    if(!params){
+        return query;
+    }else{
+        var variables = Object.keys(params).map(key => params[key]);
+        return mysql.format(query,variables);
+    }
+}
+
+
+module.exports = {
+    selectQuery
+}
